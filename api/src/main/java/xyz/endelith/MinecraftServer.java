afba4aa53cfb397ee6dec5eb431bda890b144a01
SnowyQuest <65355034+SnowyQuest@users.noreply.github.com>
2026-01-06 17:07:50 +0300

    Actually use configuration in code

--- api/src/main/java/xyz/endelith/MinecraftServer.java
+++ api/src/main/java/xyz/endelith/MinecraftServer.java
@@ -1,8 +1,11 @@
 package xyz.endelith;
 
+import xyz.endelith.configuration.ServerConfiguration;
+
 public interface MinecraftServer {
     String brandName();
     String minecraftVersion();
     int protocolVersion();
+    ServerConfiguration serverConfiguration();
     void shutdown();
 }
