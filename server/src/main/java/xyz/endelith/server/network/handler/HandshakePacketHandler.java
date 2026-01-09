a9deaf6e4645f114d27e1d0315baca1dc0a4a3f7
SnowyQuest <65355034+SnowyQuest@users.noreply.github.com>
2026-01-09 15:59:45 +0300

    Finish handshake packet

--- server/src/main/java/xyz/endelith/server/network/handler/HandshakePacketHandler.java
+++ server/src/main/java/xyz/endelith/server/network/handler/HandshakePacketHandler.java
@@ -3,6 +3,7 @@ package xyz.endelith.server.network.handler;
 import xyz.endelith.server.network.ConnectionState;
 import xyz.endelith.server.network.PlayerConnectionImpl;
 import xyz.endelith.server.network.packet.client.handshake.ClientHandshakePacket;
+import xyz.endelith.server.network.packet.client.handshake.ClientHandshakePacket.Intent;
 
 public final class HandshakePacketHandler extends PacketHandler {
 
@@ -13,7 +14,20 @@ public final class HandshakePacketHandler extends PacketHandler {
     public void handle(ClientHandshakePacket packet) {
         switch (packet.intent()) {
             case STATUS -> connection.setState(ConnectionState.STATUS);
-            case LOGIN, TRANSFER -> throw new UnsupportedOperationException("Not implemented yet");
+            case LOGIN, TRANSFER -> {
+                connection.setState(ConnectionState.LOGIN);
+                var configuration = server.configuration();
+
+                if (packet.intent() == Intent.TRANSFER) {
+                    if (!configuration.transfersAllowed()) {
+                        connection.disconnect(configuration.transfersNotAllowedMessage());
+                    }
+                }
+
+                if (packet.protocolVersion() != server.protocolVersion()) {
+                    connection.disconnect(configuration.unsupportedVersionMessage());
+                }
+            }
         }
     }
 }
