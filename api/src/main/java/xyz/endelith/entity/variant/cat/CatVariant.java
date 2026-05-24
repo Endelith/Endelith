6bd9023dea1786f3cb2bd6f1ecbe21a126147103
SnowyQuest <65355034+SnowyQuest@users.noreply.github.com>
2026-05-25 02:27:05 +0300

    Add builders

--- api/src/main/java/xyz/endelith/entity/variant/cat/CatVariant.java
+++ api/src/main/java/xyz/endelith/entity/variant/cat/CatVariant.java
@@ -4,8 +4,42 @@ import java.util.Objects;
 import net.kyori.adventure.key.Key;
 
 public record CatVariant(Key asset, Key babyAsset) {
+
     public CatVariant {
         Objects.requireNonNull(asset, "asset");
         Objects.requireNonNull(babyAsset, "baby asset");
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public Builder toBuilder() {
+        return new Builder()
+                .asset(this.asset)
+                .babyAsset(this.babyAsset);
+    }
+
+    public static final class Builder {
+
+        private Key asset;
+        private Key babyAsset;
+
+        private Builder() {
+        }
+
+        public Builder asset(Key asset) {
+            this.asset = Objects.requireNonNull(asset, "asset");
+            return this;
+        }
+
+        public Builder babyAsset(Key babyAsset) {
+            this.babyAsset = Objects.requireNonNull(babyAsset, "baby asset");
+            return this;
+        }
+
+        public CatVariant build() {
+            return new CatVariant(this.asset, this.babyAsset);
+        }
     }
 }
--- api/src/main/java/xyz/endelith/world/block/banner/BannerPattern.java
+++ api/src/main/java/xyz/endelith/world/block/banner/BannerPattern.java
@@ -4,8 +4,42 @@ import java.util.Objects;
 import net.kyori.adventure.key.Key;
 
 public record BannerPattern(Key asset, String translationKey) {
+
     public BannerPattern {
         Objects.requireNonNull(asset, "asset");
         Objects.requireNonNull(translationKey, "translation key");
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public Builder toBuilder() {
+        return new Builder()
+                .asset(this.asset)
+                .translationKey(this.translationKey);
+    }
+
+    public static final class Builder {
+
+        private Key asset;
+        private String translationKey;
+
+        private Builder() {
+        }
+
+        public Builder asset(Key asset) {
+            this.asset = Objects.requireNonNull(asset, "asset");
+            return this;
+        }
+
+        public Builder translationKey(String translationKey) {
+            this.translationKey = Objects.requireNonNull(translationKey, "translation key");
+            return this;
+        }
+
+        public BannerPattern build() {
+            return new BannerPattern(this.asset, this.translationKey);
+        }
     }
 }
